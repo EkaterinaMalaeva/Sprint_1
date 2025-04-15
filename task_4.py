729a0f20d096cf2654d42b49436eae919a1abe63
Ekaterina Malaeva <katrina.malaeva@bk.ru>
2025-04-15 23:07:31 +0300

    first commit

---
+++ task_4.py
@@ -0,0 +1,12 @@
+new_tasks = ['task_001', 'task_011', 'task_007', 'task_015', 'task_005']
+completed_tasks = ['task_002', 'task_012', 'task_006']
+
+new_tasks.pop(-1)
+completed_tasks.append('task_005')
+
+print(new_tasks)
+print(completed_tasks)
+
+new_tasks.remove('task_007')
+
+print(new_tasks[-1])
